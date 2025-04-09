094972215cccccb451f44fa0351d09c0ac2786bf
Bilal Belhadj <bilalbelhadj2025@gmail.com>
2025-04-09 12:10:34 +0100

    Use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,23 +1,3 @@
-const products = [
-    {
-        name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
-        image:'images/products/athletic-cotton-socks-6-pairs.jpg',
-        rating: {stars:4.5, count: 87},
-        price:1090
-    },
-    {
-        name:'Intermediate Size Basketball',
-        image:'images/products/intermediate-composite-basketball.jpg',
-        rating: {stars:4, count: 127},
-        price:2095
-    },
-    {
-        name:'Adults Plain Cotton T-Shirt - 2 Pack',
-        image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-        rating: {stars:4.5, count: 56},
-        price:799
-    }
-];
 let productsHTML = '';
 products.forEach((product)=>{
     productsHTML += `<div class="product-container">
@@ -39,7 +19,7 @@ products.forEach((product)=>{
           </div>
 
           <div class="product-price">
-            $${(product.price / 100).toFixed(2)}
+            $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
